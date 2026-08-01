2bab825a7a0d98c19b4afc77d5bd8062c660c92f
aaravsrivastava372-droid <aaravsrivastava372@gmail.com>
2026-08-01 19:51:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,48 @@ setTimeout(()=>{
 window.location.href="profiles.html";
 
 },4500);
+// ===============================
+// PROFILES PAGE
+// ===============================
+
+const aaratrika =
+document.getElementById("aaratrika");
+
+const dobby =
+document.getElementById("dobby");
+
+const toast =
+document.getElementById("toast");
+
+if(aaratrika){
+
+aaratrika.onclick=()=>{
+
+aaratrika.style.transform="scale(12)";
+aaratrika.style.opacity="0";
+
+setTimeout(()=>{
+
+window.location.href="home.html";
+
+},800);
+
+};
+
+}
+
+if(dobby){
+
+dobby.onclick=()=>{
+
+toast.classList.add("show");
+
+setTimeout(()=>{
+
+toast.classList.remove("show");
+
+},2500);
+
+};
+
+}
